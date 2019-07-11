b9398082a2fc538bad5bc30eb96795d03eb94088
Rami Shenouda <ramishenouda@outlook.com>
2019-07-11 02:09:56 +0200

    changed some stuff

--- timer.js
+++ timer.js
@@ -11,18 +11,18 @@ document.addEventListener('DOMContentLoaded', () =>
 
 function Timer()
 {
-    var now = new Date().getTime(); 
-    var Time = CountDownDate - now; 
+    const now = new Date().getTime(); 
+    const Time = CountDownDate - now; 
     if(Time < 0)
     {
         document.querySelector("#clock").innerHTML = "Expired";
         clearInterval(interval);
     }
 
-    var days = Math.floor(Time / (1000 * 60 * 60 * 24)); 
-    var hours = Math.floor((Time % (1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
-    var minutes = Math.floor((Time % (1000 * 60 * 60)) / (1000 * 60)); 
-    var seconds = Math.floor((Time % (1000 * 60)) / 1000); 
+    const days = Math.floor(Time / (1000 * 60 * 60 * 24)); 
+    const hours = Math.floor((Time % (1000 * 60 * 60 * 24))/(1000 * 60 * 60)); 
+    const minutes = Math.floor((Time % (1000 * 60 * 60)) / (1000 * 60)); 
+    const seconds = Math.floor((Time % (1000 * 60)) / 1000); 
 
     document.querySelector("#days").innerHTML = `${days}`;
     document.querySelector("#hours").innerHTML = `${hours}`;
